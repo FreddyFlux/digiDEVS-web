5f47761f11a847345a9135a88c575c406ad1df06
Fredrik Aarvold <fredrik@digidevs.no>
2025-03-08 23:08:06 +0100

    more google analytics config

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -5,6 +5,7 @@
 			<a href="https://github.com/freddyflux" target="_blank" class="footer-link">GitHub</a>
 			<a href="https://linkedin.com" target="_blank" class="footer-link">LinkedIn</a>
 			<a href="https://twitter.com" target="_blank" class="footer-link">Twitter</a>
+			<a href="#" class="termly-display-preferences">Consent Preferences</a>
 		</div>
 		<p>&copy; digiDEVS. All rights reserved.</p>
 	</div>
